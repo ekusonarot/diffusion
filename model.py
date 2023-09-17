a6a53b650a342d8c7688df5dc6df55244cc801b9
ekusonarot <toranosuke707@gmail.com>
2023-09-17 18:10:03 +0900

    train

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ class DIFFUSION(nn.Module):
     '''
         DENOISING DIFFUSION IMPLICIT MODELS, Jiaming Songら, 2020
     '''
-    def __init__(self, image_size=28, channels=1, start_beta=1e-4, end_beta=0.02, step=1000):
+    def __init__(self, image_size=32, channels=3, start_beta=1e-4, end_beta=0.02, step=1000):
         super(DIFFUSION, self).__init__()
         beta = torch.linspace(start_beta, end_beta, step)
         alpha = 1-beta
@@ -26,33 +26,22 @@ class DIFFUSION(nn.Module):
         )
     
     def forward(self, x, t, epsilon):
-        alpha0 = torch.sqrt(self.alpha_bar[t-1]).reshape(-1,1,1,1)
-        alpha1 = torch.sqrt(1-self.alpha_bar[t-1]).reshape(-1,1,1,1)
+        alpha0 = torch.sqrt(self.alpha_bar[t]).reshape(-1,1,1,1)
+        alpha1 = torch.sqrt(1-self.alpha_bar[t]).reshape(-1,1,1,1)
         return self.epsilon_net(x*alpha0+alpha1*epsilon, t)
 
-    def sample(self, x_t, diffusion_steps=1000, mu=0.):
-        '''
-            Eq.(12)
-            x[t-1] = √a[t-1] * (x[t] - √1-a[t] * e(x[t],t)) / √a[t] + √1-a[t-1]-sigma[t]**2 * e(x[t],t) + sigma[t]*n
-            n ~ N(0, I)
-            sigma = √(1-a[t-1])/(1-a[t]) * √1-a[t]/a[t-1]
-        '''
-        assert self.step >= diffusion_steps
-        interval = self.step//diffusion_steps
-        steps = [i*interval for i in range(diffusion_steps)]
-        steps.append(self.step-1)
-        steps.append(1)
-        steps = list(set(steps))
-        steps.sort()
-        for d_t, t in zip(reversed(steps[:-1]),reversed(steps[1:])):
+    def sample(self, x_t):
+        for t in range(self.step-1,-1,-1):
             step = torch.tensor([t], device=x_t.device, dtype=torch.float)
-            epsilon = self.epsilon_net(x_t, step)
-            d_alpha = self.alpha_bar[d_t]
-            alpha = self.alpha_bar[t]
-            sigma = torch.sqrt((1-d_alpha)/(1-alpha))*torch.sqrt(1-alpha/d_alpha)
-            pred_x0 = torch.sqrt(d_alpha)*(x_t-torch.sqrt(1.-alpha)*epsilon)/torch.sqrt(alpha)
-            to_xt = torch.sqrt(1.-d_alpha-sigma**2)*epsilon
-            noise = mu*sigma*torch.randn_like(x_t)
-            x_t = pred_x0+to_xt+noise
-            x_t = x_t.detach()
+            with torch.no_grad():
+                pred_noise = self.epsilon_net(x_t, step)
+            alpha_bar = self.alpha_bar[t]
+            alpha = self.alpha[t]
+            beta = self.beta[t]
+            x_t = 1/torch.sqrt(alpha) * (x_t - ((1-alpha)/torch.sqrt(1-alpha_bar)) * pred_noise)
+            if t > 0:
+                z = torch.sqrt(beta)*torch.randn_like(x_t)
+            else:
+                z = 0
+            x_t += z
         return x_t
--- train.py
+++ train.py
@@ -7,61 +7,71 @@ from torch.optim.lr_scheduler import LambdaLR
 import numpy as np
 from model import DIFFUSION
 from tqdm import tqdm
+import pickle
+from torchvision.utils import save_image
 from torch.utils.tensorboard import SummaryWriter
 
-transform = transforms.Compose(
-    [transforms.ToTensor(), transforms.Resize((32, 32))])
-dataset = torchvision.datasets.MNIST(root='./data', 
-                                        train=True,
-                                        download=True,
-                                        transform=transform)
-dataloader = torch.utils.data.DataLoader(dataset,
-                                            batch_size=600,
-                                            shuffle=True,
-                                            num_workers=2)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+def lr_scheduler(step):
+    if step < 100:
+        return 1.
+    elif step < 200:
+        return 0.1
+    elif step < 300:
+        return 0.01
+    else:
+        return 0.001
+    
 image_size = 32
-channels = 1
+channels = 3
 beta = 1e-3
 step = 1000
-epoch = 100
+epoch = 1000
 model = DIFFUSION(image_size, channels).to(device)
-optim = Adam(model.parameters(), lr=1e-4)
+optim = Adam(model.parameters(), lr=1e-3)
+scheduler = LambdaLR(optim, lr_lambda=lr_scheduler)
 
-def lr_lambda(epoch):
-    if epoch < 4:
-        return 10**(-2+0.5*epoch)
-    elif epoch < 30:
-        return 1.
-    elif epoch < 50:
-        return 1e-1
-    else:
-        return 1e-2
-    
-lr_scheduler = LambdaLR(optim, lr_lambda)
 if __name__ == "__main__":
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5),(0.5,0.5,0.5))])
+    dataset = torchvision.datasets.CIFAR10(root='./data', 
+                                            train=True,
+                                            download=True,
+                                            transform=transform)
+    dataloader = torch.utils.data.DataLoader(dataset,
+                                                batch_size=600,
+                                                shuffle=True,
+                                                num_workers=2)
     writer = SummaryWriter()
-    for j in range(epoch):
+    losses = [None]*epoch
+    for j in range(1,epoch+1):
+        model.train()
+        sum_loss = 0
         for i, (x, _) in enumerate(tqdm(dataloader)):
             optim.zero_grad()
             x = x.to(device)
-            x = x-0.5
             epsilon = torch.randn_like(x).to(device)
-            t = torch.randint(low=1, high=step, size=(x.size(0),)).to(device)
+            t = torch.randint(low=0, high=step-1, size=(x.size(0),)).to(device)
             epsilon_hat = model(x, t, epsilon)
             loss = F.mse_loss(epsilon, epsilon_hat)
             loss.backward()
             optim.step()
             writer.add_scalar(f"loss/{j}", float(loss), i)
-        lr_scheduler.step()
-        epsilon = torch.randn(size=(16, 1, image_size, image_size)).to(device)
-        x = model.sample(epsilon, diffusion_steps=1000, mu=1.)
-        x = (x+0.5).clamp(0., 1.)
+            sum_loss += float(loss)
+            del loss
+        losses[j-1] = sum_loss/len(dataloader)
+        scheduler.step()
+        with open("losses.pkl", "wb") as f:
+            pickle.dump(losses[:j-1], f)
+        if j % 10 != 0:
+            continue
+        model.eval()
+        epsilon = torch.randn(size=(16, channels, image_size, image_size)).to(device)
+        x = model.sample(epsilon)
+        x = (x.clamp(-1, 1) + 1)/2
         grid = torchvision.utils.make_grid(x, 4)
         writer.add_image("ddpm", grid, j)
-        x = model.sample(epsilon, diffusion_steps=100, mu=0.)
-        x = (x+0.5).clamp(0., 1.)
-        grid = torchvision.utils.make_grid(x, 4)
-        writer.add_image("ddim", grid, j)
+        save_image(grid, f"checkpoint/epoch{j}.png")
+        torch.save(model.state_dict(), f"checkpoint/epoch{j}.pth")
